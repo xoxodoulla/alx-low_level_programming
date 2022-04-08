19fc1bf5140ed653d04625784406bab66b17f503
xoxodoulla <101393237+xoxodoulla@users.noreply.github.com>
2022-04-08 14:58:53 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- *main- function begins
- *return: function ends
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-     write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return(0)
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return(1)
 }
