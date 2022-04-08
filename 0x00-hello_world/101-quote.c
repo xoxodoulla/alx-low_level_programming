b7ea7c8e0ec1071049067b7290d3278aa82ffaef
xoxodoulla <abdullahijnr69@gmail.com>
2022-04-08 06:46:26 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ *main- function begins
+ *return: function ends
+ */
+int main(void)
+{
+     write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"'59);
+return(1)
+}
